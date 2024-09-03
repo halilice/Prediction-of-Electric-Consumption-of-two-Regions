edbfa0a85eec5e538e2e33d8f6d868e65d92280f
Halil Ibrahim Celikel <169132075+halilice@users.noreply.github.com>
2024-09-03 18:27:28 +0200

    Update enedis_dashboard1.py

--- enedis_dashboard1.py
+++ enedis_dashboard1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 
 # Execute the following code to avoid displaying the numbers in scientific notation
